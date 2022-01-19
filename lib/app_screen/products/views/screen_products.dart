e827dba4f4811398abf66d1069a4e7886e9d98fb
akash <aakashthakare07@gmail.com>
2022-01-19 10:46:24 +0530

    initial start

--- lib/app_screen/products/views/screen_products.dart
+++ lib/app_screen/products/views/screen_products.dart
@@ -23,6 +23,8 @@ import 'package:fluttertoast/fluttertoast.dart';
 class ScreenProducts extends StatefulWidget {
   const ScreenProducts({Key? key}) : super(key: key);
 
+
+
   @override
   _ScreenProductsState createState() => _ScreenProductsState();
 }
